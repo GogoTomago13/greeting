b5b5b5cb105ddaa7deafad7ff4a04a7a2f0ff32c
GogoTomago13 <varshababwah@gmail.com>
2022-04-28 21:22:13 -0400

    Adds Unit_Testing activity

--- hello.js
+++ hello.js
@@ -1,3 +1,17 @@
-function hello() {
-  return "Hello World!";
+function greet(name) {
+  let greeting = "Hello " + name + "!";
+  if (name === null){
+    name = 'Hello There!'
+  }
+  if (name.toUpperCase === name){ 
+    greeting = "HELLO " + name.toUpperCase;
+  }
+  function add(name1){ 
+    greeting = greeting + ", " + name1;
+  }
+  if (Array.isArray(name)){ 
+    greeting = "Hello"
+    name.forEach(add);
+  }
+  return greeting;
 }
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,22 @@
 var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+var greet = utils.load('./hello.js', 'greet');
+
+//test('outputs the correct string', () => {
+  //expect(hello()).toBe("Hello World!");
+//});
+
+//var utils  = require('course-utilities');
+//var hello = utils.load('./hello.js', 'hello');
 
 test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+  expect(greet('Luna')).toBe("Hello Luna!");
 });
+
+
+
+//describe('test hello()', function() {
+    //it ('should be called', function (){
+     //   expect(hello('Luna')).toBeCalled();
+   // });
+//});
+
